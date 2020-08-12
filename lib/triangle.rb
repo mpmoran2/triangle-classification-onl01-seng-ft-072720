9ee115a88c6e3d8fd87d24b6cfde12197f72c880
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-12 19:43:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  attr_reader :sides
+
+  def initialize(sX, sY, sZ)
+    @sX = sX
+    @sY = sY
+    @sZ = sZ
+    @sides = [sX, sY, sZ]
+  end
+
+  def valid_triangle?
+    ((@sides[0] + @sides[1] > @sides[2]) && (@sides[0] + @sides[2] > @sides[1]) && (@sides[1] + @sides[2] > @sides[0])) && @sides.all? {|side| side > 0}
+  end
+
+
+  def kind()
+    if valid_triangle? != true
+    raise TriangleError
+    else
+      if valid_tri? == true && ((@sides[0] == @sides[1]) && (@sides[0] == @sides[2]))
+        :equilateral
+      elsif valid_tri? == true && ((@sides[0] == @sides [1]) || (@sides[0] == @sides [2]) || (@sides[1] == @sides [2]))
+        :isosceles
+      elsif valid_tri? == true && @sides.uniq == @sides
+        :scalene
+      end
+    end
+  end 
+
+  class TriangleError < StandardError
+    def error_msg
+      puts "This is no triangle!"
+    end 
+  end
 end
+
+
+#Define :equilateral :isosceles :scalene
